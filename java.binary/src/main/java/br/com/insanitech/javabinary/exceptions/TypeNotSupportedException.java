9ab0a461ac7fce235dd98175b64dc2faa96cdd6a
Anderson Lucas C. Ramos <anderson@insanitech.com.br>
2017-06-27 20:47:38 -0300

    Implemented Encodable class;

--- java.binary/src/main/java/br/com/insanitech/javabinary/exceptions/TypeNotSupportedException.java
+++ java.binary/src/main/java/br/com/insanitech/javabinary/exceptions/TypeNotSupportedException.java
@@ -1,12 +1,13 @@
 package br.com.insanitech.javabinary.exceptions;
 
+import java.io.IOException;
 import java.util.Locale;
 
 /**
  * Created by anderson on 22/06/2017.
  */
 
-public class TypeNotSupportedException extends Exception {
+public class TypeNotSupportedException extends IOException {
     public TypeNotSupportedException(Class<?> type) {
         super(String.format(Locale.getDefault(), "Type %s is not supported yet.", type.toString()));
     }
